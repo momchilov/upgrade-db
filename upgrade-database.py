da5bd0f3d6003d57e82fac6c7d7e8b684c206793
Kiril Momchilov <kiril@admins-MacBook-Pro.local>
2017-04-06 10:56:09 +0100

    make the regex more precise to match the files

--- upgrade-database.py
+++ upgrade-database.py
@@ -33,7 +33,7 @@ def main():
     files.sort()
 
     for file in files:
-        match = re.search('(^[0-9.]+)(\.)?', file)
+        match = re.search('^(\d+(?:\.\d+)+?\d +?)\.?\w+\.sql$', file)
 
         found = match.group(1) if match else None
         print("Found is " + found)
